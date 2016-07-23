6cb45f757a2fa33cc19ad79869d36465fb93cb17
madD7 <sdhaval.3003@gmail.com>
2016-07-23 13:39:33 +0530

    repetative timer interrupt

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <time.h>
+#include <signal.h>
+#include <sys/time.h>
 #include <math.h>
 #include <inttypes.h>
+#include <string.h>
 
 volatile uint32_t timerTicks=0;
 
@@ -34,15 +36,32 @@ void* tickInit()
 	return NULL;
 }
 
-
-
+void timerhandler(int signum)
+{
+	printf("timerticks=%d\n", timerTicks++);
+}
 
 
 int main()
 {
-	pthread_t tidTimer;
+	struct itimerval timer;
+	struct sigaction act;
+	
+	/* setup handler for SigAlrm */
+	memset(&act, 0, sizeof(act));
+	act.sa_handler = &timerhandler;
+	sigaction(SIGVTALRM, &act, NULL);
+	
+	/* Setup interval timer */
+	/* Timer is configured to expire after 5mSec */
+	timer.it_value.tv_sec = 0;
+	timer.it_value.tv_usec = 100000;
 
-	pthread_create(&tidTimer, NULL, &tickInit, NULL);
+	/* 5mSec repetatively */
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_usec = 100000;
+
+	setitimer(ITIMER_VIRTUAL, &timer, NULL);
 
 	while(1);
 
